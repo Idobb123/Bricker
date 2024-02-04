1516167ba5ddd91b8b6737006be58bdd2f71cf0c
idobenzvibrenner <ido.benzvibrenner@mail.huji.ac.il>
2024-02-04 14:49:43 +0200

    Added the strategy classes

---
+++ src/bricker/brick_strategies/AdditionalStrikeStrategy.java
@@ -0,0 +1,10 @@
+package bricker.brick_strategies;
+
+import danogl.GameObject;
+
+public class AdditionalStrikeStrategy implements CollisionStrategy{
+    @Override
+    public void onCollision(GameObject object1, GameObject object2) {
+
+    }
+}
