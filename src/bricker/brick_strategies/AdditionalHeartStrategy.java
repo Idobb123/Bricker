a251b7a77c0b178cd7877f89600d9c761af00a3f
Adam Fleisher <adamflei123@gmail.com>
2024-02-12 18:46:45 +0200

    added docs to all the strategies

--- src/bricker/brick_strategies/AdditionalHeartStrategy.java
+++ src/bricker/brick_strategies/AdditionalHeartStrategy.java
@@ -3,13 +3,30 @@ package bricker.brick_strategies;
 import bricker.BrickerGameManager;
 import danogl.GameObject;
 
+/**
+ * A class representing a strategy that allows the user to gain an extra heart by braking a brick with this strategy.
+ * @author Ido Ben Zvi Brenner & Adam Leon Fleisher
+ */
 public class AdditionalHeartStrategy extends StrategyDecorator{
     private BrickerGameManager brickerGameManager;
 
+    /**
+     * Creates an instance for the additional heart strategy.
+     * The class also applies any other strategy given.
+     * @param collisionStrategy Another collision strategy to apply.
+     * @param brickerGameManager The game manager instance.
+     */
     public AdditionalHeartStrategy(CollisionStrategy collisionStrategy, BrickerGameManager brickerGameManager) {
         super(collisionStrategy);
         this.brickerGameManager = brickerGameManager;
     }
+
+    /**
+     * The function is responsible for applying the class strategy on a given brick when called.
+     * The function deletes the brick and creates a falling heart object using the brickerGameManager.
+     * @param brick The brick to apply the strategy on.
+     * @param other The object that collided with the brick.
+     */
     @Override
     public void onCollision(GameObject brick, GameObject other) {
         super.onCollision(brick, other);
--- src/bricker/brick_strategies/PuckStrategy.java
+++ src/bricker/brick_strategies/PuckStrategy.java
@@ -3,13 +3,28 @@ package bricker.brick_strategies;
 import bricker.BrickerGameManager;
 import danogl.GameObject;
 import danogl.util.Vector2;
-
+/**
+ * A class representing a strategy that creates two "puck" balls by braking a brick with this strategy.
+ * @author Ido Ben Zvi Brenner & Adam Leon Fleisher
+ */
 public class PuckStrategy extends StrategyDecorator{
     private BrickerGameManager brickerGameManager;
+    /**
+     * Creates an instance for the additional "puck balls" strategy.
+     * The class also applies any other strategy given.
+     * @param collisionStrategy Another collision strategy to apply.
+     * @param brickerGameManager The game manager instance.
+     */
     public PuckStrategy(CollisionStrategy collisionStrategy,BrickerGameManager brickerGameManager) {
         super(collisionStrategy);
         this.brickerGameManager = brickerGameManager;
     }
+    /**
+     * The function is responsible for applying the class strategy on a given brick when called.
+     * The function deletes the brick and creates 2 additional "puck balls" using the brickerGameManager.
+     * @param brick The brick to apply the strategy on.
+     * @param other The object that collided with the brick.
+     */
     @Override
     public void onCollision(GameObject brick, GameObject other) {
         super.onCollision(brick, other);
